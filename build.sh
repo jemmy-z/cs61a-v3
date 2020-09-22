6af1ae4ac835d9c179e4351c1657c3b2abd32179
Jemmy Zhou <jemmyzhou@berkeley.edu>
2020-09-21 22:42:51 -0700

    updated for week 5

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ declare -a ANNOUNCEMENT_FILES
 declare -a NAVBAR_FILES
 
 SEMESTER="Fall 2020"
-WEEKS=(1 2 3)
+WEEKS=(1 2 3 4)
 SURVEY="https://forms.gle/R6o4ch6bFjiuScAaA"
 VERSION="v1.0.1"
 
@@ -45,5 +45,5 @@ echo "Finished updating version"
 cp temp/* .
 echo "Finished publishing all files"
 
-rm about.html-*
-rm index.html-*
+# rm about.html-*
+# rm index.html-*
